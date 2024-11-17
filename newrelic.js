67249f39c4c5065d94a65934a0e773db65bfadb6
amodh <amodhjaiswal@gmail.com>
2024-11-17 22:07:04 +0530

    first

---
+++ newrelic.js
@@ -0,0 +1,7 @@
+exports.config = {
+    app_name: ['node'],
+    license_key: 'eu01xxd779cc580faa7d4c15b8367906FFFFNRAL',
+    logging: {
+        level: 'info', // Adjust as needed (debug, trace, warn, etc.)
+    },
+};
