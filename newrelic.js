f0310aa40c9a7c4e35fb6ba14cbbcb6dc82229dc
amodh <amodhjaiswal@gmail.com>
2024-11-18 14:55:37 +0530

    final

--- newrelic.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-    app_name: ['node'],
-    license_key: 'eu01xxd779cc580faa7d4c15b8367906FFFFNRAL',
-    logging: {
-        level: 'info', // Adjust as needed (debug, trace, warn, etc.)
-    },
-};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.js
-require('./newrelic');
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
